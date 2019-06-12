9eac7308e86c47caaa7e8cc9e9b4447b5eb62cc2
root <root@localhost.localdomain>
2019-06-12 10:20:55 +0100

    prod

--- dist/config.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require('dotenv').config();
-exports.uri = `mongodb://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.PORT}/${process.env.APP_NAME}`;
-//# sourceMappingURL=config.js.map
